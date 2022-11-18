2b6fb633bb753b5f1986efd70e85d0635a9c8514
Lucas Feijo <lucas.feijo@gmail.com>
2022-11-18 19:41:43 -0300

    Initial Commit

---
+++ Taggers/TaggersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaggersApp.swift
+//  Taggers
+//
+//  Created by Lucas C. Feijo on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TaggersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
